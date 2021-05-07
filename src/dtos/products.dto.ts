031dce4348522f96b6c73a0914ed5baa313d3d8d
maestro_roshi <ccdelgadop@gmail.com>
2021-05-06 22:54:36 -0500

    add validation to dtos

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,14 +1,38 @@
 // Dto: data transfer object to recive from request
 // This is util to expience of develoment but in exection time it no valida
 
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty
+} from 'class-validator'
+
+// documentation
+// https://trilon.io/blog/introducing-mapped-types-for-nestjs
+
 /**
  * Dto to create product
  */
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
@@ -16,6 +40,7 @@ export class CreateProductDto {
  * Dto to update product
  */
 export class UpdateProductDto {
+  @IsString()
   readonly name?: string;
   readonly description?: string;
   readonly price?: number;
